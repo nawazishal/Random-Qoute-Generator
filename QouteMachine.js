31c1443908c1502bf9e8dcfb88ea7f594f5b43cf
Nawazish Ali <50316441+nawazishal@users.noreply.github.com>
2019-08-06 16:26:03 +0500

    implemented hooks and used functional component.

--- QouteMachine.js
+++ QouteMachine.js
@@ -1,30 +1,33 @@
-import React, { useState,useEffect } from 'react';
+   
+import React, { useState,useEffect} from 'react';
 import axios from 'axios'
 
 export default function Qoutes() {
-     const [qoute, setqoute] = useState({})
-     const [id, setid] = useState(1)
-     const [btnId,setbtnId]=useState(1)
-
-     useEffect(()=>{
-         axios.get(`https://jsonplaceholder.typicode.com/todos/${btnId}`)
+     const [qoute, setqoute] = useState([])
+     const [author, setauthor] = useState('')
+     
+ useEffect(()=>{
+     document.title= qoute
+ })
+ 
+    const getQoute= (()=>{
+         axios.get("https://api.quotable.io/random")
          .then(response => {
-             setqoute(response.data)
+             setqoute(response.data.content)
+             setauthor(response.data.author)
          })
         .catch((error)=>{
             console.log(error)
         })
-     },[btnId])
-    const  hanldeinput=()=>{
-      setbtnId(id)
-     }
+     })
     
     return (
-        <div>
-            <input type="text" value={id} onChange={e => setid(e.target.value)} />
-            <button onClick={hanldeinput}>fetch qoute</button>
-            <h1>{qoute.title} </h1>
+        <React.Fragment>
+            <h1>Qoute Generator</h1>
+            <button onClick={getQoute}>fetch qoute</button>
+            <textarea value={qoute}/>
+            <p>Qoute By : {author}</p>
             <button className="tweet-qoute"  onClick={()=>{window.open("https://twitter.com/intent/tweet"); alert('successful')}}>Tweet Qoute</button>
-        </div>
+        </React.Fragment>
     )
 }
